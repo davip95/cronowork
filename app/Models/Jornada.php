05752a9985f3b2869992aa62bd0720a23ad378b1
jefe <jefe@daw.es>
2023-05-04 19:44:18 +0200

    modelo y migracion tabla jornadas

---
+++ app/Models/Jornada.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Jornada extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'jornadas';
+    protected $fillable = [
+        'dia',
+        'minutos_descanso',
+        'hora_inicio',
+        'hora_fin',
+    ];
+
+    public $timestamps = false;
+}
---
+++ database/migrations/2023_05_04_174127_create_jornadas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJornadasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jornadas', function (Blueprint $table) {
+            $table->id();
+            $table->string('dia', 1);
+            $table->integer('minutos_descanso');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            $table->unsignedBigInteger('horarios_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jornadas');
+    }
+}
