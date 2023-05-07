df4fa6fd630d8ac9d8f661d5d82e3ae67c4a0b23
jefe <jefe@daw.es>
2023-05-07 14:54:07 +0200

    sanctum use eliminado

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes;
 
     protected $dates = ['fecha_nacimiento', 'fecha_alta'];
 
