cfe9203241b0fc0695b9e14d971d69c6ee46dc96
jefe <jefe@daw.es>
2023-06-13 17:16:12 +0200

    configuracion despliegue aplicacion

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -19,8 +19,8 @@ window.bootstrap = bootstrap
  */
 
 window.axios = require('axios');
-//window.axios.defaults.baseURL = 'https://ieslamarisma.net/proyectos/2023/davidperez/cronowork1/public';
-window.axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+window.axios.defaults.baseURL = 'https://ieslamarisma.net/proyectos/2023/davidperez/cronowork/public';
+// window.axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
